9d05fe3176dee03b940ec15504deed8f895a1a26
Gabriel Duarte <gabriel.duarte.z.gd@gmail.com>
2026-07-11 03:48:32 -0400

    Clase 8

--- publicar.R
+++ publicar.R
@@ -1,7 +1,7 @@
 # publicar.R - corre esto cada vez que quieras actualizar el sitio
 system("quarto render")
 system('git add .')
-system('git commit -m "Clase 7"')
+system('git commit -m "Clase 8"')
 system('git push origin main')
 
 Cartografia_censo2024_Pais_Manzanas.parquet
